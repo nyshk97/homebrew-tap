1767c2602ba0a64282cf8732dfc02c0251827fa6
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2025-03-23 20:23:33 +0900

    update version

--- Formula/rmtrash-mac.rb
+++ Formula/rmtrash-mac.rb
@@ -1,9 +1,9 @@
 class RmtrashMac < Formula
   desc "A safer rm: Move files to the macOS Trash, not permanently delete."
   homepage "https://github.com/nyshk97/rmtrash-mac"
-  url "https://github.com/nyshk97/rmtrash-mac/archive/v0.2.0.tar.gz"
-  sha256 "780c852b34cbbab7a74909052424df77810cc83a2e8f04ffdf4e9f680e2288ed"  # 既存のハッシュ値
-  version "0.2.0"
+  url "https://github.com/nyshk97/rmtrash-mac/archive/v0.2.2.tar.gz"
+  sha256 "b83a50666cb5cd5a6f6d288ff4546c4a2b08fa27e719f2cf56ee31177c9f827e"
+  version "0.2.2"
   license "MIT"
 
   depends_on "rust" => :build
