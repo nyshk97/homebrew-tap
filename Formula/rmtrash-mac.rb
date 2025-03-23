74396de22d7dda279d0ef9217aeee6e1656b34d5
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2025-03-23 20:15:11 +0900

    Update command

--- Formula/rmtrash-mac.rb
+++ Formula/rmtrash-mac.rb
@@ -1,19 +1,19 @@
 class RmtrashMac < Formula
   desc "A safer rm: Move files to the macOS Trash, not permanently delete."
   homepage "https://github.com/nyshk97/rmtrash-mac"
-  url "https://github.com/nyshk97/rmtrash-mac/archive/v0.1.0.tar.gz"
-  sha256 "3360a428f37ad060fcb87b0319651389fd06346ab3f2b1a15db3f6bab3b6df24"
-  version "0.1.0"
+  url "https://github.com/nyshk97/rmtrash-mac/archive/v0.2.0.tar.gz"
+  sha256 "780c852b34cbbab7a74909052424df77810cc83a2e8f04ffdf4e9f680e2288ed"  # 既存のハッシュ値
+  version "0.2.0"
   license "MIT"
 
   depends_on "rust" => :build
   depends_on :macos
 
   def install
-    system "cargo", "install", "--root", prefix, "--path", "."
+    system "cargo", "install", "--root", prefix, "--path", ".", "--bin", "rmtrash"
   end
 
   test do
-    system "#{bin}/trash", "--version"
+    system "#{bin}/rmtrash", "--version"
   end
 end
