d1d2048d85970ccd21d86a2c5fcd5d9f26ddb297
tsubasa namatame <nyshk97@gmail.com>
2026-04-01 23:21:30 +0900

    Update todo-mac to v1.2.0

--- Casks/todo-mac.rb
+++ Casks/todo-mac.rb
@@ -1,6 +1,6 @@
 cask "todo-mac" do
-  version "1.1.0"
-  sha256 "ca79f227ced23d3ca2be24c25d9ee61289240f1ab1513681a20063b250a2b610"
+  version "1.2.0"
+  sha256 "f71287205cf45dc1b35df05b356c31de0d3013c9ffa3ae61f5ab8b318d4d0121"
 
   url "https://github.com/nyshk97/todo-app/releases/download/v#{version}/TodoMac.zip"
   name "TodoMac"
