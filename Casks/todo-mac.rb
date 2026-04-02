3b5a2fe8058893cd89356b7e75a1d7d8751a2e04
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2026-04-02 20:59:30 +0900

    bump todo-mac to 1.4.0

--- Casks/todo-mac.rb
+++ Casks/todo-mac.rb
@@ -1,6 +1,6 @@
 cask "todo-mac" do
-  version "1.3.0"
-  sha256 "f71287205cf45dc1b35df05b356c31de0d3013c9ffa3ae61f5ab8b318d4d0121"
+  version "1.4.0"
+  sha256 "411c3fa3e5088cd6698fd16b698dfcf7ce9ec1226ec5fef17fb1b1008eaf2e15"
 
   url "https://github.com/nyshk97/todo-app/releases/download/v#{version}/TodoMac.zip"
   name "TodoMac"
