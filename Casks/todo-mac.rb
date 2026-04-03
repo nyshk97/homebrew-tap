a4fd4637ec57ce9a5cd0ffa26689aeb163bba95f
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2026-04-03 16:01:39 +0900

    chore: bump todo-mac to 1.11.0

--- Casks/todo-mac.rb
+++ Casks/todo-mac.rb
@@ -1,6 +1,6 @@
 cask "todo-mac" do
-  version "1.10.1"
-  sha256 "5d810a062ab6138209abf3d144e54d58367a7c668876346325f71e97f62d98c4"
+  version "1.11.0"
+  sha256 "578f0a44412ad9c5fed95e38d3c034cf584afc22f71a8c911dabd8aa2a67b281"
 
   url "https://github.com/nyshk97/todo-app/releases/download/v#{version}/TodoMac.zip"
   name "TodoMac"
