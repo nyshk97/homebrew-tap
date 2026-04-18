1f46eaa3938160a7b25733d30821c22cafb8717e
tsubasa namatame <nyshk97@gmail.com>
2026-04-18 16:34:21 +0900

    chore: diff-viewer 1.7.1

--- Casks/diff-viewer.rb
+++ Casks/diff-viewer.rb
@@ -1,6 +1,6 @@
 cask "diff-viewer" do
-  version "1.7.0"
-  sha256 "3d993890b6f02afb0fea71c5a2a0ec9c6ef6d9bb7050a5261619dff33a1e7f40"
+  version "1.7.1"
+  sha256 "5720ede07a3cbc2ab8753f2719b0606bb2fe4f994cc7680e888d6826b02da26b"
 
   url "https://github.com/nyshk97/diff-viewer/releases/download/v#{version}/DiffViewer.zip"
   name "DiffViewer"
