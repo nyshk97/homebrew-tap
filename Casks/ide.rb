e4ad849e146acc4ab9f0f42bf9b46986ef0b1198
tsubasa namatame <nyshk97@gmail.com>
2026-05-10 14:27:16 +0900

    ide: 0.0.3

--- Casks/ide.rb
+++ Casks/ide.rb
@@ -1,11 +1,11 @@
 cask "ide" do
-  version "0.0.2"
-  sha256 "304622a81dd67ed00934d30b44dbd256d7b3d02ab33e3236d953abf415d4a695"
+  version "0.0.3"
+  sha256 "ffce2bccfbfe7763eecf5cd20632cf1e0ac2c1183730e22980cfc8e45c60c4e4"
 
   url "https://github.com/nyshk97/ide/releases/download/v#{version}/ide.zip"
   name "ide"
   desc "Self-hosted IDE that integrates Ghostty terminal and Claude Code"
   homepage "https://github.com/nyshk97/ide"
 
-  app "ide.app"
+  app "IDE.app"
 end
