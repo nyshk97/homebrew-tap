55997e0a0c17e99af9641f45514fb5ec54424a70
tsubasa namatame <nyshk97@gmail.com>
2026-05-11 14:18:14 +0900

    ide 0.0.6

--- Casks/ide.rb
+++ Casks/ide.rb
@@ -1,6 +1,6 @@
 cask "ide" do
-  version "0.0.5"
-  sha256 "d4c6dfcfa482a4ce9083d90e5cb743d6a2147c50df6c1e52ef11fbdd08b3c645"
+  version "0.0.6"
+  sha256 "03655c7c57822c1753ee2cfcd45c3045a5a7d0298edeece4643eabd0b040313e"
 
   url "https://github.com/nyshk97/ide/releases/download/v#{version}/ide.zip"
   name "ide"
