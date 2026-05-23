a48bf7c9643cd6881f5dc3ad550366246ff40de6
tsubasa namatame <nyshk97@gmail.com>
2026-05-23 19:00:17 +0900

    polepole: 1.0.2

--- Casks/polepole.rb
+++ Casks/polepole.rb
@@ -1,6 +1,6 @@
 cask "polepole" do
-  version "1.0.1"
-  sha256 "ba02536b150084aa036183de417b03f6742bdc21c1faf07b90d6ca85f7a825e5"
+  version "1.0.2"
+  sha256 "5e8fb392abecae8ef0646f884f91f91f40792fe901af0a77b65e10f58f441535"
 
   url "https://github.com/nyshk97/polepole-releases/releases/download/v#{version}/polepole.zip"
   name "PolePole"
