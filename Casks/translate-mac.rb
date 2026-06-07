5ac5a7491f260ac28749c82893949d496f276042
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2026-06-07 23:28:28 +0900

    chore: translate-mac 0.1.2

--- Casks/translate-mac.rb
+++ Casks/translate-mac.rb
@@ -1,15 +1,15 @@
 cask "translate-mac" do
-  version "0.1.1"
-  sha256 "a3f40bbd072caa5724a9d97add3fd6083dcc34946354430269e66deaf5740925"
+  version "0.1.2"
+  sha256 "7148073eb8fc8babda8a17128720ca739574458ef0e63e71eeaa477a09f9f6a2"
 
-  url "https://github.com/nyshk97/translate/releases/download/v#{version}/Translate.zip"
-  name "Translate"
+  url "https://github.com/nyshk97/translate/releases/download/v#{version}/Translator.zip"
+  name "Translator"
   desc "自分専用の macOS ネイティブ翻訳ツール"
   homepage "https://github.com/nyshk97/translate"
 
   depends_on macos: ">= :sonoma"
 
-  app "Translate.app"
+  app "Translator.app"
 
   zap trash: [
     "~/Library/Application Support/com.d0ne1s.translate",
