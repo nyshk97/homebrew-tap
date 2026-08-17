92171d6029fc446a712791e5c99a1e0d9744a0ed
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2026-08-18 08:41:59 +0900

    chore: menubar-tidy 1.0.1

--- Casks/menubar-tidy.rb
+++ Casks/menubar-tidy.rb
@@ -1,6 +1,6 @@
 cask "menubar-tidy" do
-  version "1.0.0"
-  sha256 "1fc24eab7ce03a91304b12c26f3aeebcb0d00b9de7916341c0e0d94a6d602a9e"
+  version "1.0.1"
+  sha256 "0825e6811102db720d9e6eb4aa9fe24694d431e6449436aca0aff04174c8e96e"
 
   url "https://github.com/nyshk97/menubar-tidy/releases/download/v#{version}/MenuBarTidy-#{version}.zip"
   name "MenuBar Tidy"
