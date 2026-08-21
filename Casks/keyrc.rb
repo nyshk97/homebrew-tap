86233027d0f93360f73d2fee9f2ae1e2ce56fea1
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2026-08-21 23:11:31 +0900

    chore: keyrc 0.1.2

--- Casks/keyrc.rb
+++ Casks/keyrc.rb
@@ -1,6 +1,6 @@
 cask "keyrc" do
-  version "0.1.1"
-  sha256 "d8934219eb3b041555ea5a163212fea4ba71a44a04c0b345a2c450b4a2e26b9d"
+  version "0.1.2"
+  sha256 "c4b3f12a102c9a53139ef9cb8e78b458c19782ea306483e3792e6984773fe122"
 
   url "https://github.com/nyshk97/keyrc/releases/download/v#{version}/keyrc-#{version}.zip"
   name "keyrc"
