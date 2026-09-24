c69034a98e4bcc1238a416aad97785b8b05a37c3
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2026-09-24 20:13:28 +0900

    chore: mycast 0.1.2

--- Casks/mycast.rb
+++ Casks/mycast.rb
@@ -1,6 +1,6 @@
 cask "mycast" do
-  version "0.1.1"
-  sha256 "6cd2b0ee80eea6bbc202c44de8ff430af713523a66ff97279d14cfd3a8b16add"
+  version "0.1.2"
+  sha256 "459157b6610c2f542db7fa1e27b57789744b69c320568947616a6776175158d9"
 
   url "https://github.com/nyshk97/mycast/releases/download/v#{version}/mycast-#{version}.zip"
   name "mycast"
