62d0b1a8513c74ee533bba5e38ea29a46a556063
Tsubasa Namatame <38527299+nyshk97@users.noreply.github.com>
2026-09-26 15:55:57 +0900

    chore: mycap 0.5.0

--- Casks/mycap.rb
+++ Casks/mycap.rb
@@ -1,6 +1,6 @@
 cask "mycap" do
-  version "0.4.0"
-  sha256 "91062886fed2f4ff6cb0c934463c651653153876a81e0408a16731f7fc6090ba"
+  version "0.5.0"
+  sha256 "4701af8ccb1b23ac38a06e19ec11bbf71e1a5b98b4500d4a8d4c23b50c099b5a"
 
   url "https://github.com/nyshk97/mycap/releases/download/v#{version}/mycap-#{version}.zip"
   name "mycap"
